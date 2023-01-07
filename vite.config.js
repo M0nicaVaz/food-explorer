06a363c58a59f34932ef756d558ae49acc5fff2a
Monica Vaz <contatomonicavaz@gmail.com>
2023-01-07 16:58:25 -0300

    chore: config wsl

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react()],
   css: {
     preprocessorOptions: {
