cb0f5ee771fbce57b057cc9e82630931ff0ea545
Monica Vaz <monica.vaz@eumedicoresidente.com>
2023-06-24 12:57:06 -0300

    fix: admin navabr

--- src/components/AdminHeader/index.jsx
+++ src/components/AdminHeader/index.jsx
@@ -4,7 +4,6 @@ import {
   X,
   ShoppingCartSimple,
 } from 'phosphor-react'
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Logo } from '../Logo'
 import styles from './header.module.scss'
@@ -12,7 +11,6 @@ import styles from './header.module.scss'
 import { useAuth } from '../../context/useAuth'
 
 export function AdminHeader() {
-  const [isOpen, setIsOpen] = useState(false)
   const { signOut } = useAuth()
 
   return (
@@ -22,32 +20,9 @@ export function AdminHeader() {
         <Link to="/">
           <Logo variant={'small'} />
         </Link>
-
         <span>admin</span>
       </div>
 
-      <div className={styles.buttons}>
-        {/* {isOpen ? (
-          <span>
-            <X size={24} />
-          </span>
-        ) : (
-          <button
-            title="Menu"
-            className={styles.btnMenu}
-            data-count={amountOfItemsInCart}
-            onClick={() => {
-              setIsOpen(true)
-            }}
-          >
-            <ShoppingCartSimple size={24} />
-          </button>
-        )} */}
-
-        <button title="Sair" className={styles.btnLogOut} onClick={signOut}>
-          <SignOut size={24} />
-        </button>
-      </div>
 
       <div className={styles.search}>
         <MagnifyingGlass size={22} />
@@ -55,9 +30,16 @@ export function AdminHeader() {
       </div>
 
 
-      <Link to="/admin/new" className={`${styles.orderBtn} ${styles.desktopOnly}`}>
+      <Link to="/admin/new" className={`${styles.orderBtn}`}>
         Novo prato
       </Link>
+
+      <div className={styles.buttons}>
+        <button title="Sair" className={styles.btnLogOut} onClick={signOut}>
+          <SignOut size={24} />
+        </button>
+      </div>
+
     </header>
   )
 }
