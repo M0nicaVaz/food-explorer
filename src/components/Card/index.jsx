b1fde48c7d58442792f667fac09e98499cd5dd91
Monica Vaz <monica.vaz@eumedicoresidente.com>
2023-06-25 13:07:15 -0300

    fix(Card): rendering accordingly with type of user

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,35 @@
 import { Link } from 'react-router-dom';
 import { Form } from '../Form';
 import { formatPrice } from '../../utils/formatPrice';
+import { useAuth } from '../../context/useAuth';
 
 import styles from './card.module.scss';
+import { PencilSimple } from 'phosphor-react';
 
 export function Card({ data, ...rest }) {
+  const { isAdmin } = useAuth();
+
   return (
     <article className={styles.wrapper} {...rest}>
+      {isAdmin ?
+        <Link className={styles.editBtn} to={`admin/new?product=${data.id}`}>
+          <PencilSimple size={28} />
+        </Link>
+        : null}
+
       <img src={data.src} alt="" />
 
-      <Link to={`/details/${data.id}`}>
-        <strong className={styles.title}>{data.title}</strong>
-      </Link>
+      <div className={styles.textWrapper} >
+        <Link to={`/details/${data.id}`}>
+          <strong className={styles.title}>{data.title}</strong>
+        </Link>
 
-      <span className={styles.subtitle}>{data.description}</span>
+        <span className={styles.subtitle}>{data.description}</span>
 
-      <span className={styles.price}>{formatPrice(data.price)}</span>
+        <span className={styles.price}>{formatPrice(data.price)}</span>
+      </div>
 
-      <Form data={data} />
+      {isAdmin ? null : <Form data={data} />}
     </article>
   );
 }
