15c50dcd08385ca5064acd91a24d36180f9e9bd3
Monica Vaz <monica.vaz@eumedicoresidente.com>
2023-06-25 13:08:44 -0300

    fix(Details): rendering accordingly with type of user

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,7 +5,7 @@ import { CaretLeft } from 'phosphor-react';
 import { Form } from '../../components/Form';
 import { api } from '../../services/api';
 import { formatPrice } from '../../utils/formatPrice';
-import { useAuth } from '../../context/useAuth'
+import { useAuth } from '../../context/useAuth';
 
 export function Details() {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,60 +21,68 @@ export function Details() {
   useEffect(() => {
     async function getProduct() {
       try {
-        const { data } = await api.get(`/products/${id}`)
-        const productImageUrl = `${api.defaults.baseURL}/files/${data.image}`
+        const { data } = await api.get(`/products/${id}`);
+        const productImageUrl = `${api.defaults.baseURL}/files/${data.image}`;
 
         if (data) {
-          setProduct({ ...data, src: productImageUrl })
-          setIsLoading(false)
+
+          setProduct({ ...data, src: productImageUrl });
+          setIsLoading(false);
         }
-      }
-      catch (error) {
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
     }
 
-    getProduct()
-  }, [])
+    getProduct();
+  }, []);
 
   return (
-
     <>
-      {
-        isLoading ? <span className={styles.loading} >Carregando...</span> :
-          <main className={styles.content}>
+      {isLoading ? (
+        <span className={styles.loading}>Carregando...</span>
+      ) : (
+        <main className={styles.content}>
+          <button className={styles.backBtn} onClick={handleGoBack}>
+            <CaretLeft />
+            voltar
+          </button>
 
-            <button className={styles.backBtn} onClick={handleGoBack}>
-              <CaretLeft />
-              voltar
-            </button>
+          <div className={styles.details}>
+            <img className={styles.mainImg} src={product.src} alt="" />
 
-            <div className={styles.details}>
-              <img className={styles.mainImg} src={product.src} alt="" />
+            <div className={styles.contentWrapper}>
+              <h1 className={styles.title}>{product.title}</h1>
 
-              <div className={styles.contentWrapper}>
-                <h1 className={styles.title}>{product.title}</h1>
+              <span className={styles.subtitle}>{product.description}</span>
 
-                <span className={styles.subtitle}>{product.description}</span>
+              {product.ingredients.length > 0 && (
+                <div className={styles.ingredients}>
+                  {product.ingredients.map((ingredient) => (
+                    <span key={ingredient.id}>{ingredient.name}</span>
+                  ))}
+                </div>
+              )}
 
-                {
-                  isAdmin ?
-                    <Link to={`/admin/new?product=${id}`} className={`${styles.orderBtn}`}>
-                      Editar prato
-                    </Link>
-                    :
-                    <footer>
-                      <span className={styles.price}>{formatPrice(product.price)}</span>
-                      <Form data={product} className={styles.stepper} />
-                    </footer>
-                }
-
-              </div>
+              {isAdmin ? (
+                <Link
+                  to={`/admin/new?product=${id}`}
+                  className={`${styles.orderBtn}`}
+                >
+                  Editar prato
+                </Link>
+              ) : (
+                <footer>
+                  <span className={styles.price}>
+                    {formatPrice(product.price)}
+                  </span>
+                  <Form data={product} className={styles.stepper} />
+                </footer>
+              )}
             </div>
-          </main>
-      }
-
+          </div>
+        </main>
+      )}
     </>
-
   );
 }
